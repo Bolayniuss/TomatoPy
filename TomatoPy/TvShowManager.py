fe16f3952eaa5e8d14210800286a3cbbbc63cfb1
Bolay Michael <michael.bolay@gmail.com>
2014-01-07 11:56:48 +0100

    Error fix

--- TomatoPy/TvShowManager.py
+++ TomatoPy/TvShowManager.py
@@ -12,7 +12,7 @@ import time
 
 class TvShowManager(AutomatedActionsExecutor):
 	def __init__(self, torrentManager):
-		super(AutomatedActionsExecutor, self).__init__("TvShowManager")
+		super(AutomatedActionsExecutor, self).__init__(actionNotifierName="TvShowManager")
 		dbm = DatabaseManager.Instance()
 		self.torrentManager = torrentManager
 
