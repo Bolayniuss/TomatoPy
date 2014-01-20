c8dcb2a53553398db1a006eb43887a554cecf8eb
Bolay Michael <michael.bolay@gmail.com>
2014-01-20 14:06:17 +0100

    fix error

--- TomatoPy/SourceMapper.py
+++ TomatoPy/SourceMapper.py
@@ -4,19 +4,24 @@ __author__ = 'bolay'
 import os
 from TomatoPy.Filters import *
 from TomatoPy.SourceMapperItem import *
+import sys
 
 
 class DirectoryMapper:
 
 	def __init__(self, path, filter=FileFilter()):
-		self.path = u""+path
+		self.path = (u""+path).encode("latin-1")
 		self.filter = filter
 		self.files = []
 		self.map()
 
 	def map(self):
-		for root, dirs, files in os.walk(self.path):
+		fsEncoding = sys.getfilesystemencoding()
+		path = self.path.encode(fsEncoding)
+		for root, dirs, files in os.walk(path):
 			for file in files:
+				file = file.decode(fsEncoding)
+				root = root.decode(fsEncoding)
 				print file, type(file)
 				item = FileItem(file, root)
 				if self.filter.test(item):
