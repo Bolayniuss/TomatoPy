a344d56ed063e6fcf6fd81dcd7173f285f54bebb
Bolayniuss <michael.bolay@gmail.com>
2015-11-29 20:59:13 +0100

    Add UnitTest tests

--- TomatoPy/tests/scrapper_tests.py
+++ TomatoPy/tests/scrapper_tests.py
@@ -13,11 +13,13 @@ class BetaSeriesTestCase(unittest.TestCase):
         self.episodes = self.scrapper.getEpisodes()
 
     def test_betaseries(self):
-        self.assertEqual(len(self.episodes), 56, "")
+        self.assertEqual(len(self.episodes), 28, "")
 
     def test_list(self):
-        for e in self.episodes:
-            print e
+        print self.episodes[0].title
+		print self.episodes[0].tvShow
+		print self.episodes[0].season
+		print self.episodes[0].episodeNumber
 
 
 if __name__ == '__main__':
