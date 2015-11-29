9cc63f7754d7e6b5e6b0e9cd5a0d301e37afbdf9
Bolayniuss <michael.bolay@gmail.com>
2015-11-29 20:57:34 +0100

    Add UnitTest tests

--- TomatoPy/tests/scrapper_tests.py
+++ TomatoPy/tests/scrapper_tests.py
@@ -9,10 +9,10 @@ from TomatoPy.Scrapper import BetaserieRSSScrapper
 
 class BetaSeriesTestCase(unittest.TestCase):
     def setUp(self):
-        self.scrapper = BetaserieRSSScrapper("bolayniuss_test")
+        self.scrapper = BetaserieRSSScrapper("bolaynius_test")
         self.episodes = self.scrapper.getEpisodes()
-    def test_betaseries(self):
 
+    def test_betaseries(self):
         self.assertEqual(len(self.episodes), 56, "")
 
     def test_list(self):
