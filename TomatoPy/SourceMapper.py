c37b22101f01451c21964a84860c0aad41d83ee7
Bolayniuss <michael.bolay@gmail.com>
2015-11-29 21:16:02 +0100

    Re indent file

--- TomatoPy/SourceMapper.py
+++ TomatoPy/SourceMapper.py
@@ -1,15 +1,14 @@
 # -*- coding: utf8 -*-
 __author__ = 'bolay'
 
-import os
-import re
 import sys
 
 from .Filters import *
 from .SourceMapperItem import *
 
-#old class
-#class DirectoryMapper:
+
+# old class
+# class DirectoryMapper:
 #
 #	def __init__(self, path, filter=FileFilter(), encoding=None):
 #		self.path = path
@@ -35,35 +34,34 @@ from .SourceMapperItem import *
 
 
 class DirectoryMapper:
+    def __init__(self, path, filter_=r"", encoding=None):
+        self.path = path
+        if encoding is not None:
+            self.path = path.encode(encoding)
+        self.filter = re.compile(filter_)
+        self.files = []
+        self.indexedFiles = {}
+        self.map()
 
-	def __init__(self, path, filter_=r"", encoding=None):
-		self.path = path
-		if encoding is not None:
-			self.path = path.encode(encoding)
-		self.filter = re.compile(filter_)
-		self.files = []
-		self.indexedFiles = {}
-		self.map()
+    def map(self):
+        reload(sys)
+        sys.setdefaultencoding('UTF8')
+        for root, dirs, files in os.walk(self.path):
+            for file_ in files:
+                m = self.filter.match(file_)
+                if m:
+                    item = FileItem(m.group(1), m.group(2), root)
+                    self.files.append(item)
+                    key = file_[0].lower()
+                    if key not in self.indexedFiles:
+                        self.indexedFiles[key] = []
+                    self.indexedFiles[key].append(item)
 
-	def map(self):
-		reload(sys)
-		sys.setdefaultencoding('UTF8')
-		for root, dirs, files in os.walk(self.path):
-			for file_ in files:
-				m = self.filter.match(file_)
-				if m:
-					item = FileItem(m.group(1), m.group(2), root)
-					self.files.append(item)
-					key = file_[0].lower()
-					if key not in self.indexedFiles:
-						self.indexedFiles[key] = []
-					self.indexedFiles[key].append(item)
-
-	def fileExists(self, file_):
-		loweredFile = file_.lower()
-		key = loweredFile[0]
-		if key in self.indexedFiles:
-			for item in self.indexedFiles[key]:
-				if item.name.lower() == loweredFile:
-					return True
-		return False
+    def fileExists(self, file_):
+        lowered_file = file_.lower()
+        key = lowered_file[0]
+        if key in self.indexedFiles:
+            for item in self.indexedFiles[key]:
+                if item.name.lower() == lowered_file:
+                    return True
+        return False
