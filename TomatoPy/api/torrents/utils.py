974aa47af98f1ec50670615ec9a4214d7d2e7101
Bolayniuss <michael.bolay@gmail.com>
2016-12-16 16:20:37 +0100

    remove print

--- TomatoPy/api/torrents/utils.py
+++ TomatoPy/api/torrents/utils.py
@@ -4,7 +4,6 @@ from __future__ import print_function, absolute_import, unicode_literals
 
 import bencode
 import hashlib
-import base64
 import urllib
 
 
@@ -12,18 +11,11 @@ def magnet_from_data(torrent_data):
     metadata = bencode.bdecode(torrent_data)
 
     hash_contents = bencode.bencode(metadata['info'])
-    digest = hashlib.sha1(hash_contents).digest()
     hex_digest = hashlib.sha1(hash_contents).hexdigest()
-    #b32hash = base64.b32encode(digest)
-
-    print(hex_digest)
-    import json
-    print(json.dumps(metadata, indent=4, ensure_ascii=False))
 
     params = [
         ("xt", hex_digest),
         ("dn", metadata['info']['name']),
-        #("xl", metadata['info']['length'])
     ]
 
     announces = metadata["announce"]
@@ -33,8 +25,6 @@ def magnet_from_data(torrent_data):
     for an in announces:
         params.append(("tr", an))
 
-    print(params)
-
     encoded_params = []
     for k, v in params:
         v = urllib.quote_plus(bytes(v), safe=b":")
@@ -42,7 +32,6 @@ def magnet_from_data(torrent_data):
             v = b'urn:btih:%s' % v
         encoded_params.append("%s=%s" % (k, v))
 
-    #param_str = urllib.urlencode(params)
     magnet_uri = 'magnet:?%s' % "&".join(encoded_params)
     return magnet_uri
 
